9e822ad535598a6f17e0eded9bbfab59b20d74d0
Avinash <avinash.shinde@goqii.com>
2025-09-24 14:02:10 +0530

    added project changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bhhaiii... successfully deployed first react app on azure using git
         </p>
         <a
           className="App-link"
